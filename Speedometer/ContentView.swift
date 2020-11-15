bad7f51b1aa7d0058752439162e3fb2f83647e68
ajisahputra78 <ajisaputro.s.kom@gmail.com>
2020-11-16 02:08:12 +0700

    Initial Commit

---
+++ Speedometer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Speedometer
+//
+//  Created by Aji_sahputra on 16/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
